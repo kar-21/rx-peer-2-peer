39749bb2f0aa25cdd613875b4bb9f92105ac70ab
kar-21 <karthiks210198@gmail.com>
2021-07-15 20:27:26 +0530

    adding path

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
